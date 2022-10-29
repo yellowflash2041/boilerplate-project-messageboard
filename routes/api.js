faf12d0d3fad0d50da2a5bb40142a306e1740bd6
yellowflash2041 <yellowflash2041@gmail.com>
2022-10-30 02:49:28 +0900

    Final Update

--- routes/api.js
+++ routes/api.js
@@ -42,11 +42,18 @@ module.exports = function (app) {
             container._id = message._id;
             container.created_on = message.created_on;
             container.bumped_on = message.bumped_on;
-            container.reported = message.reported;
             container.text = message.text;
-            container.delete_password = message.delete_password;
             container.replycount = message.replycount;
-            container.replies = message.replies.slice(0, 3);
+            const repliesConsolidate = message.replies.slice(0, 3).map(reply => {
+              const replyContainer = {};
+
+              replyContainer._id = reply._id;
+              replyContainer.created_on = reply.created_on;
+              replyContainer.text = reply.text;
+
+              return replyContainer;
+            });
+            container.replies = repliesConsolidate;
 
             return container;
           });
@@ -55,7 +62,7 @@ module.exports = function (app) {
     })
     .post(async (req, res) => {
       const newMessage = new Message({
-        board: req.body.board,
+        board: req.params.board,
         created_on: new Date().toISOString(),
         bumped_on: new Date().toISOString(),
         reported: false,
@@ -77,14 +84,14 @@ module.exports = function (app) {
       });
     })
     .put((req, res) => {
-      Message.findByIdAndUpdate(mongoose.Types.ObjectId(req.body.thread_id), { reported: true }, { useFindAndModify: false }, (err, docs) => {
+      Message.findOneAndUpdate({ board: req.params.board, _id: mongoose.Types.ObjectId(req.body.report_id) }, { reported: true }, { useFindAndModify: false }, (err, docs) => {
         if (err) {
           throw err;
         }
         if (!docs) {
           res.send('invalid id');
         } else {
-          res.send('success');
+          res.send('reported');
         }
       });
     })
@@ -111,32 +118,43 @@ module.exports = function (app) {
           if (err) {
             console.log(err);
           }
-          res.json(messages[0]);
+          const container = {};
+
+          container._id = messages[0]._id;
+          container.created_on = messages[0].created_on;
+          container.bumped_on = messages[0].bumped_on;
+          container.text = messages[0].text;
+          container.replycount = messages[0].replycount;
+          const repliesConsolidate = messages[0].replies.map(reply => {
+            const replyContainer = {};
+
+            replyContainer._id = reply._id;
+            replyContainer.created_on = reply.created_on;
+            replyContainer.text = reply.text;
+
+            return replyContainer;
+          });
+          container.replies = repliesConsolidate;
+
+          res.json(container);
         });
     })
-    .post((req, res) => {
-      Message.findOne({ board: req.body.board, _id: mongoose.Types.ObjectId(req.body.thread_id) }, async (err, doc) => {
-        try {
-          if (err) {
-            console.log(err);
-          }
-          const newReply = {
-            created_on: new Date().toISOString(),
-            text: req.body.text,
-            delete_password: req.body.delete_password,
-            reported: false
-          };
-          doc.replycount = doc.replycount + 1;
-          doc.replies.push(newReply);
-          await doc.save();
-          res.json(doc);
-        } catch (err) {
-          console.error(err);
-        }
+    .post(async (req, res) => {
+      const newReply = new Reply({
+        created_on: new Date().toISOString(),
+        text: req.body.text,
+        delete_password: req.body.delete_password,
+        reported: false
       });
+      const doc = await Message.findOne({ board: req.params.board, _id: mongoose.Types.ObjectId(req.body.thread_id) });
+      doc.bumped_on = newReply.created_on;
+      doc.replycount = doc.replycount + 1;
+      doc.replies.push(newReply);
+      await doc.save();
+      res.json(doc);
     })
     .put((req, res) => {
-      Message.findOne({ board: req.body.board, _id: mongoose.Types.ObjectId(req.body.thread_id) }, async (err, doc) => {
+      Message.findOne({ board: req.params.board, _id: mongoose.Types.ObjectId(req.body.thread_id) }, async (err, doc) => {
         if (err) {
           throw err;
         }
@@ -151,13 +169,13 @@ module.exports = function (app) {
           } else {
             doc.replies[result].reported = true;
             await doc.save();
-            res.send('success');
+            res.send('reported');
           }
         }
       });
     })
     .delete((req, res) => {
-      Message.findOne({ board: req.body.board, _id: mongoose.Types.ObjectId(req.body.thread_id) }, async (err, doc) => {
+      Message.findOne({ board: req.params.board, _id: mongoose.Types.ObjectId(req.body.thread_id) }, async (err, doc) => {
         if (err) {
           throw err;
         }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -31,7 +31,6 @@ suite('Functional Tests', function() {
           assert.equal(res.status, 200);
           assert.isAtMost(res.body.length, 10);
           assert.isAtMost(res.body[0].replies.length, 3);
-          assert.exists(res.body[0].delete_password);
         });
     }),
     test("Deleting a thread with the incorrect password: DELETE request to '/api/threads/{board}' with an invalid 'delete_password'", () => {
@@ -89,10 +88,10 @@ suite('Functional Tests', function() {
             .put(`/api/threads/${board}`)
             .send({
               board: board,
-              thread_id: mongoose.Types.ObjectId(res.body._id)
+              report_id: mongoose.Types.ObjectId(res.body._id)
             })
             .end((error, response) => {
-              assert.equal(response.text, 'success');
+              assert.equal(response.text, 'reported');
             });
         });
     }),
@@ -100,7 +99,6 @@ suite('Functional Tests', function() {
       chai.request(server)
         .post(`/api/threads/${board}`)
         .send({
-          board: board,
           text: 'Creating a new thread to create a new reply with: POST request',
           delete_password: 'replied'
         })
@@ -108,27 +106,31 @@ suite('Functional Tests', function() {
           chai.request(server)
             .post(`/api/replies/${board}`)
             .send({
-              board: board,
               thread_id: mongoose.Types.ObjectId(res.body._id),
               text: 'This is the new reply to the new thread: POST request',
               delete_password: 'thereply'
             })
             .end((error, response) => {
+              const no = response.body.replies.length - 1;
               assert.equal(response.status, 200);
-              assert.equal(response.body.replies.length, 1);
-              assert.equal(response.body.replies[0].text, 'This is the new reply to the new thread: POST request');
-              assert.equal(response.body.replies[0].delete_password, 'thereply');
+              assert.equal(response.body.replies[no].text, 'This is the new reply to the new thread: POST request');
+              assert.equal(response.body.replies[no].delete_password, 'thereply');
               assert.equal(res.body.text, 'Creating a new thread to create a new reply with: POST request');
             });
         });
     }),
     test("Viewing a single thread with all replies: GET request to '/api/replies/{board}'", () => {
       chai.request(server)
-        .get(`/api/replies/${board}`)
+        .get(`/api/threads/${board}`)
         .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.exists(res.body.text);
-          assert.exists(res.body.replies);
+          chai.request(server)
+            .get(`/api/replies/${board}`)
+            .send({ thread_id: mongoose.Types.ObjectId(res.body[0]._id) })
+            .end((err2, res2) => {
+              assert.equal(res2.status, 200);
+              assert.exists(res2.body.text);
+              assert.exists(res2.body.replies);
+            });
         });
     }),
     test("Deleting a reply with the incorrect password: DELETE request to '/api/threads/{board}' with an invalid 'delete_password'", () => {
@@ -221,7 +223,7 @@ suite('Functional Tests', function() {
                   reply_id: mongoose.Types.ObjectId(res2.body.replies[0]._id)
                 })
                 .end((err3, res3) => {
-                  assert.equal(res3.text, 'success');
+                  assert.equal(res3.text, 'reported');
                 });
             });
         });
